401dba44fb45596c12f1ecf06dbcb30f9fc878f6
mengyuan1112 <yuanmeng@buffalo.edu>
2022-03-12 14:59:32 -0500

    complete Log/Reg UI

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,14 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from "./component/Login";
+import Reg from "./component/Reg";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route exact path='/Login' element={<Login/>}>
-          </Route>
+          <Route exact path='/Login' element={<Login/>}></Route>
+          <Route exact path='/Reg' element={<Reg/>}></Route>
         </Routes>
       </BrowserRouter>
     </div>
